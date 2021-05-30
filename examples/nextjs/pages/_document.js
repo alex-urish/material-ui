68d4c09d7dfee267d9de6c28396414e152a069f9
Alex Urish <alex-urish@mossmap.com>
2021-05-30 08:14:11 -0500

    NextJS SSR Injection Order Issue w/ ServerStyleSheets

--- examples/nextjs/pages/_document.js
+++ examples/nextjs/pages/_document.js
@@ -60,7 +60,9 @@ MyDocument.getInitialProps = async (ctx) => {
   // 4. page.render
 
   // Render app and page and get the context of the page with collected side effects.
-  const sheets = new ServerStyleSheets();
+  const sheets = new ServerStyleSheets({
+    injectFirst: true
+  });
   const originalRenderPage = ctx.renderPage;
 
   const cache = getCache();
